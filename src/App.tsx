310425da9b68fe3e55f4add44865e490995bb2a3
SierraDjunfan <52620271+SierraDjunfan@users.noreply.github.com>
2024-05-13 07:56:29 +1000

    Adjusted Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div id="screen">
-      <h1>HUNGRY FELLAS</h1>
-      <p>COMING SOON</p>
+      <div id="heading">
+        <h1>HUNGRY FELLAS</h1>
+        <p>COMING SOON</p>
+      </div>
       <video autoPlay loop muted playsInline id="gameplay-video">
         <source src="gameplay.mp4" type="video/mp4" />
         Your browser does not support the video tag.
